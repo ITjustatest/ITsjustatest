306bf89c78d27275715a44663b21243da2b3b2bd
JoannaPe <39209371+JoannaPe@users.noreply.github.com>
2018-05-12 16:58:58 +0300

    My comment

--- src/projecttest/ProjectTest.java
+++ src/projecttest/ProjectTest.java
@@ -17,6 +17,7 @@ public class ProjectTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello");
+        System.out.println("My new commit");
         
     }
     
