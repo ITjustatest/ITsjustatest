84947f00fc2748bd8a6663a3e3df34abb8e18a5d
Kostas <kostaskontopoulos5@windowslive.com>
2018-05-12 16:40:25 +0300

    New Change, just a print on the main method

--- trunk/src/projecttest/ProjectTest.java
+++ trunk/src/projecttest/ProjectTest.java
@@ -21,6 +21,7 @@ public class ProjectTest {
         System.out.println("Last Changes");
         System.out.println("Hello Joanna, Andromeda, this is user 3");
         System.out.println("Whatever I quit");
+        System.out.println("Smoking that is");
 
     }
     
