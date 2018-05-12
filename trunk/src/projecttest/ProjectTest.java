3d4494b2329299bc5e092b53f3da16375f5b3b1c
Kostas <kostaskontopoulos5@windowslive.com>
2018-05-12 16:26:20 +0300

    New Change, just a print on the main method

---
+++ trunk/src/projecttest/ProjectTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecttest;
+
+/**
+ *
+ * @author Joanna P
+ */
+public class ProjectTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Helloto the Team");
+        System.out.println("Helloto the Team");
+        System.out.println("Last Changes");
+        System.out.println("Hello, Joanna, Andromeda, I am user 3");
+    }
+    
+}
